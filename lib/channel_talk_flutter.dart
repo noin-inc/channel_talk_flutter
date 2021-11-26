a455c7132cccb61979b5c6dd0dfd7c8f97243a84
Yasuyo Nishioka <yasuyo.nishioka@noin.tv>
2021-11-26 10:34:10 +0900

    fix conflict

--- lib/channel_talk_flutter.dart
+++ lib/channel_talk_flutter.dart
@@ -120,31 +120,6 @@ class ChannelTalk {
   }
 
   static Future<bool?> openChat({
-    required String chatId,
-    required String message,
-  }
-
-  static Future<bool?> shutdown() {
-    return _channel.invokeMethod('shutdown');
-  }
-
-  static Future<bool?> showChannelButton() {
-    return _channel.invokeMethod('showChannelButton');
-  }
-
-  static Future<bool?> hideChannelButton() {
-    return _channel.invokeMethod('hideChannelButton');
-  }
-
-  static Future<bool?> showMessenger() {
-    return _channel.invokeMethod('showMessenger');
-  }
-
-  static Future<bool?> hideMessenger() {
-    return _channel.invokeMethod('hideMessenger');
-  }
-
-  static Future<bool?> openChat({
     String? chatId,
     String? message,
   }) {
